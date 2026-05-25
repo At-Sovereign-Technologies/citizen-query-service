e60a139fd5ea91ca64aa6df69884bcfc7c1b5933
Santiago <mesasantiago5@gmail.com>
2026-05-24 22:59:37 -0500

    test: skip integration tests locally when Docker unavailable (assume Docker)

--- src/test/java/com/electoral/citizen_query_service/AbstractIntegrationTest.java
+++ src/test/java/com/electoral/citizen_query_service/AbstractIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.electoral.citizen_query_service;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assumptions;
+import org.testcontainers.DockerClientFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -10,6 +12,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class AbstractIntegrationTest {
+    @BeforeAll
+    public static void assumeDockerAvailable() {
+        Assumptions.assumeTrue(DockerClientFactory.instance().isDockerAvailable(), "Docker not available - skipping integration tests");
+    }
 
     @Container
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
